90ea5fe351ef4d4b7327410d7f681b95821e21b7
Bl4ck4venger <39086464+Bl4ck4venger@users.noreply.github.com>
2019-02-19 03:39:20 +0100

    Finaly commit

---
+++ android-memory-master/app/src/main/java/android/math/uni/lodz/pl/memory/InstructionsActivity.java
@@ -0,0 +1,18 @@
+package android.math.uni.lodz.pl.memory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Game instructions activity.
+ *
+ * @author Maciej Cieplucha.
+ */
+public class InstructionsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+}
